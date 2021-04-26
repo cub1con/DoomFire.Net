c0b3f1920f33fdad318d26511e256aafe973aafe
Jan Kesteloo <18596772+cub1con@users.noreply.github.com>
2021-04-26 23:42:13 +0200

    Fixed Errors

--- src/DoomFire.Net/DoomFire.Net/DoomFire.cs
+++ src/DoomFire.Net/DoomFire.Net/DoomFire.cs
@@ -1,125 +1,185 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Drawing;
-using System.Timers;
+using System.Threading;
 using Color = System.Drawing.Color;
 
 namespace DoomFireNet
 {
     public class DoomFire
     {
-        public Bitmap BITMAP { get; private set; }
+        public Bitmap Frame { get; private set; }
 
-        public event EventHandler FrameRenderd;
+        private int FramesRendered;
+
+        private int _fps;
+
+        public int FPS
+        {
+            get { return _fps;}
+            private set
+            {
+                _fps = value;
+                OnFpsUpdate();
+            }
+        }
+
+        public int TotalFramesRendered { get; private set; }
+
+        public event EventHandler<Bitmap> FrameRenderd;
+
+        public event EventHandler FpsUpdated;
+
+        public DateTime RunningSince { get; private set; } = new DateTime();
 
         public int Width { get; private set; }
         public int Height { get; private set; }
-        public Timer Timer { get; private set; }
+        public long MaxTicksPerSecond { get; private set;}
 
-        public Color[] FIRE_PAL { get; private set; } = new Color[37];
-        public int[] FIRE_PIXELS { get; private set; }
-        public int[] FIRE_RGB { get; private set; } = {
+        private Random random = new Random();
+
+        public Color[] FireColors { get; private set; } = new Color[37];
+        public int[] FirePixels { get; private set; }
+        private readonly int[] FireRgb = 
+        {
             0x07, 0x07, 0x07, 0x1F, 0x07, 0x07, 0x2F, 0x0F, 0x07, 0x47, 0x0F, 0x07, 0x57, 0x17, 0x07, 0x67,
             0x1F, 0x07, 0x77, 0x1F, 0x07, 0x8F, 0x27, 0x07, 0x9F, 0x2F, 0x07, 0xAF, 0x3F, 0x07, 0xBF, 0x47,
             0x07, 0xC7, 0x47, 0x07, 0xDF, 0x4F, 0x07, 0xDF, 0x57, 0x07, 0xDF, 0x57, 0x07, 0xD7, 0x5F, 0x07,
             0xD7, 0x5F, 0x07, 0xD7, 0x67, 0x0F, 0xCF, 0x6F, 0x0F, 0xCF, 0x77, 0x0F, 0xCF, 0x7F, 0x0F, 0xCF,
             0x87, 0x17, 0xC7, 0x87, 0x17, 0xC7, 0x8F, 0x17, 0xC7, 0x97, 0x1F, 0xBF, 0x9F, 0x1F, 0xBF, 0x9F,
             0x1F, 0xBF, 0xA7, 0x27, 0xBF, 0xA7, 0x27, 0xBF, 0xAF, 0x2F, 0xB7, 0xAF, 0x2F, 0xB7, 0xB7, 0x2F,
-            0xB7, 0xB7, 0x37, 0xCF, 0xCF, 0x6F, 0xDF, 0xDF, 0x9F, 0xEF, 0xEF, 0xC7, 0xFF, 0xFF, 0xFF };
+            0xB7, 0xB7, 0x37, 0xCF, 0xCF, 0x6F, 0xDF, 0xDF, 0x9F, 0xEF, 0xEF, 0xC7, 0xFF, 0xFF, 0xFF
+        };
 
 
 
-        public DoomFire(int width, int height, int tps)
+        DateTime _lastTime; // marks the beginning the measurement began
+
+        public DoomFire(int width, int height, int targetFps)
         {
             this.Height = height;
             this.Width = width;
-            this.FIRE_PIXELS = new int[Width * Height];
+            this.FirePixels = new int[Width * Height];
 
-            this.BITMAP = new Bitmap(Width, Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            this.Frame = new Bitmap(Width, Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
 
-            this.Timer = new Timer(1000 / tps);
-            this.Timer.Elapsed += _timer_Elapsed;
-            this.Timer.AutoReset = true;
+            this.MaxTicksPerSecond = TimeSpan.TicksPerSecond / targetFps;
 
             // Fill Color pallete
-            for (var i = 0; i < FIRE_PAL.Length; i++)
+            for (var i = 0; i < this.FireColors.Length; i++)
             {
-                FIRE_PAL[i] = Color.FromArgb((byte)FIRE_RGB[i * 3 + 0], (byte)FIRE_RGB[i * 3 + 1], (byte)FIRE_RGB[i * 3 + 2]);
+                this.FireColors[i] = Color.FromArgb((byte)this.FireRgb[i * 3 + 0], (byte)this.FireRgb[i * 3 + 1], (byte)this.FireRgb[i * 3 + 2]);
             }
 
             // Preset Frame
             for (var i = 0; i < Width * Height; i++)
             {
-                FIRE_PIXELS[i] = 0;
+                this.FirePixels[i] = 0;
             }
 
             // Preset "fire source" in frame
             for (var i = 0; i < Width; i++)
             {
-                FIRE_PIXELS[(Height - 1) * Width + i] = 36;
+                this.FirePixels[(Height - 1) * Width + i] = 36;
             }
-
-            this.Timer.Enabled = true;
         }
 
-        protected virtual void OnFrameRendered(EventArgs e)
+        public void RenderFramesAtInterval()
         {
+            this.RunningSince = DateTime.Now;
+            while (true)
+            {
+                var startTicks = Environment.TickCount;
+                RenderFrame();
+                var workTicks = Environment.TickCount - startTicks;
+                var remainingTicks = MaxTicksPerSecond - workTicks;
+                Thread.Sleep(Math.Max(Convert.ToInt32(remainingTicks / 10000), 0));
+            }
+        }
+
+        public void RenderFrames()
+        {
+            this.RunningSince = DateTime.Now;
+            while (true)
+            {
+                RenderFrame();
+            }
+        }
+
+        protected virtual void OnFpsUpdate()
+        {
+            var handler = FpsUpdated;
+            handler?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnFrameRendered(Bitmap frame)
+        {
+            Debug.Print($"Rendered Frame {this.TotalFramesRendered}");
             var handler = FrameRenderd;
-            handler?.Invoke(this, e);
+            handler?.Invoke(this, frame);
         }
 
 
         private void DoFire()
         {
-            for (var x = 0; x < Width; x++)
+            for (var x = 0; x < this.Width; x++)
             {
-                for (var y = 1; y < Height; y++)
+                for (var y = 1; y < this.Height; y++)
                 {
-                    SpreadFire(y * Width + x);
+                    this.SpreadFire(y * this.Width + x);
                 }
             }
         }
 
-        private void SpreadFire(int from)
+        private void SpreadFire(int src)
         {
-            var to = FIRE_PIXELS[from];
+            var to = this.FirePixels[src];
 
             if (to == 0)
             {
-                FIRE_PIXELS[from - Width] = 0;
+                this.FirePixels[src - Width] = 0;
                 return;
             }
 
-            to = from - Width;
-            FIRE_PIXELS[to] = FIRE_PIXELS[from] - 1;
+            var rand = (random.Next() * 3) & 3;
+
+            to = (src - Width) + 1 - rand;
+            if (to < 0)
+                to = 0;
+
+            this.FirePixels[to] = this.FirePixels[src] - (rand & 1);
         }
 
         private int DrawPixel(int x, int y, int pixel)
         {
-            //if (x >= BITMAP.Height || y >= BITMAP.Width)
-            //{
-            //    throw new ArgumentOutOfRangeException($"DrawPixel at x:{x} y:{y} while Bitmap is x:{BITMAP.Height} y:{BITMAP.Width}");
-            //}
-
-            BITMAP.SetPixel(x, y, FIRE_PAL[pixel]);
+            this.Frame.SetPixel(x, y, FireColors[pixel]);
             return pixel;
         }
 
-        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        public void RenderFrame()
         {
-            DoFire();
+            this.DoFire();
 
-
-            for (var h = 0; h < Height; h++)
+            for (var h = 0; h < this.Height; h++)
             {
-                for (var w = 0; w < Width; w++)
+                for (var w = 0; w < this.Width; w++)
                 {
-                    var color = FIRE_PIXELS[h * Width + w];
-                    DrawPixel(w, h, color);
+                    var color = this.FirePixels[h * Width + w];
+                    this.DrawPixel(w, h, color);
                 }
             }
 
-            OnFrameRendered(EventArgs.Empty);
+
+            this.FramesRendered++;
+            this.TotalFramesRendered++;
+            
+            this.OnFrameRendered(Frame);
+
+            if (!((DateTime.Now - _lastTime).TotalSeconds >= 1)) return;
+            // one second has elapsed 
+            FPS = this.FramesRendered;
+            this.FramesRendered = 0;
+            _lastTime = DateTime.Now;
         }
     }
 }
--- src/DoomFire.Net/DoomFire.Net/MainWindow.xaml.cs
+++ src/DoomFire.Net/DoomFire.Net/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Windows;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 
 namespace DoomFireNet
 {
@@ -16,27 +19,55 @@ namespace DoomFireNet
         public MainWindow()
         {
             InitializeComponent();
-            //this.Fire = new DoomFire(this.ImgFire.Width, this.ImgFire.Height, 60);
-            this.Fire = new DoomFire(200, 100, 60);
-            this.Fire.FrameRenderd += Fire_FrameRenderd;
+
+            this.Loaded += delegate(object sender, RoutedEventArgs args)
+            {
+                //this.Fire = new DoomFire((int)this.ActualWidth, (int)this.ActualHeight, 27);
+                this.Fire = new DoomFire(256, 144, 27);
+                this.Fire.FrameRenderd += Fire_FrameRenderd;
+                this.Fire.FpsUpdated += Fire_FpsUpdated;
+                this.Fire.RenderFramesAtInterval();
+            };
         }
 
-        private void Fire_FrameRenderd(object sender, EventArgs e)
+        private void Fire_FpsUpdated(object sender, EventArgs e)
         {
-            this.ImgFire.Source = Convert(Fire.BITMAP);
+            this.Title = $"DoomFire.Net ~({this.Fire.FPS}fps / {this.Fire.TotalFramesRendered}frames total  / running for {DateTime.Now - this.Fire.RunningSince})";
+        }
+
+        private void Fire_FrameRenderd(object sender, Bitmap frame)
+        {
+            var convertedFrame = Convert(frame);
+
+            Dispatcher.Invoke(() =>
+            {
+                this.ImgFire.Source = convertedFrame;
+            }, DispatcherPriority.ContextIdle);
         }
 
 
         public BitmapImage Convert(Bitmap src)
         {
+            var sp = new Stopwatch();
+            sp.Start();
+
             var ms = new MemoryStream();
-            ((System.Drawing.Bitmap)src).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            src.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
             var image = new BitmapImage();
             image.BeginInit();
             ms.Seek(0, SeekOrigin.Begin);
             image.StreamSource = ms;
             image.EndInit();
+
+            sp.Stop();
+            Debug.WriteLine($"Converting Image took {sp.Elapsed}");
+
             return image;
+        }
+
+        private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
